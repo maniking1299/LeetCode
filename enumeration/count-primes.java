9331ddb1d0d184fb5d8b31d30b1cbc14e196461d
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-04-14 10:24:08 +0530

    LC-count-primes [Runtime: 94 ms; Memory: 51.2 MB]

--- enumeration/count-primes.java
+++ enumeration/count-primes.java
@@ -10,7 +10,7 @@ class Solution {
         isPrime[0] =false;
         isPrime[1] = false;
         
-        for(int i=2 ;i<n ;i++){
+        for(int i=2 ;i*i<n ;i++){
             if(isPrime[i]){
                 for(int j=i*i ;j<n ;j+=i){
                     isPrime[j] = false;
