73335951eb1c52a2220b081442ff70573c0367b5
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-04-16 20:41:29 +0530

    LC-sum-of-subarray-minimums [Runtime: 62 ms; Memory: 59.5 MB]

---
+++ monotonic-stack/sum-of-subarray-minimums.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        // TC->O(N)
+        // SC->O(N)
+         int n = arr.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        // LEFT: strictly greater
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+                stack.pop();
+            }
+            left[i] = stack.isEmpty() ? i + 1 : i - stack.peek();
+            stack.push(i);
+        }
+
+        stack.clear();
+
+        // RIGHT: greater or equal
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+            right[i] = stack.isEmpty() ? n - i : stack.peek() - i;
+            stack.push(i);
+        }
+
+        long sum = 0;
+        int MOD = 1_000_000_007;
+
+        for (int i = 0; i < n; i++) {
+            long contribution = (long) arr[i] * left[i] * right[i];
+            sum = (sum + contribution) % MOD;
+        }
+
+        return (int) sum;
+    
+    }
+}
