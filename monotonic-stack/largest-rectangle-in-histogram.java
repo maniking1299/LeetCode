ca1785261db06d187fa3d6b6d9567978a1d235f2
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-04-21 21:02:51 +0530

    LC-largest-rectangle-in-histogram [Runtime: N/A; Memory: N/A]

---
+++ monotonic-stack/largest-rectangle-in-histogram.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int h = heights.length;
+        int lb[] = new int[h];
+        int rb[] = new int[h];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0 ;i<h ;i++){
+            while(!stack.isEmpty() && heights[stack.peek()] > heights[i]){
+                stack.pop();
+            }
+            lb[i] = stack.isEmpty()?-1:stack.peek();
+            stack.push(i);
+        }
+        for(int i=h-1 ;i>=0 ;i--){
+            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
+                stack.pop();
+            }
+            rb[i] = stack.isEmpty()?h:stack.peek();
+            stack.push(i);
+        }
+
+        int maxArea = -1;
+
+        for(int i=0 ;i<h ;i++){
+            maxArea  = Math.max(maxArea , (rb[i]-lb[i]-1)*heights[i]);
+        }
+
+        return maxArea;
+    }
+}
