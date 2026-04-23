3c11d2d2e580b2041855fd643339f3736c0a1971
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-04-23 20:54:27 +0530

    LC-sliding-window-maximum [Runtime: 26 ms; Memory: 143.4 MB]

---
+++ heap-priority-queue/sliding-window-maximum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> q = new ArrayDeque<>();
+        int[] ans = new int[nums.length-k+1];
+
+        for(int i=0 ; i<k ; i++){
+            while(!q.isEmpty() && nums[q.peekLast()]<=nums[i]){
+                q.pollLast();
+            }
+            q.offerLast(i);
+        }
+
+        ans[0] = nums[q.peekFirst()];
+
+        for(int i=k ;i<nums.length ;i++){
+
+            if(q.peekFirst() <= i-k){
+                q.pollFirst();
+            }
+
+            while(!q.isEmpty() && nums[q.peekLast()]<=nums[i]){
+                q.pollLast();
+            }
+
+            q.offerLast(i);
+
+            ans[i-k+1] = nums[q.peekFirst()];
+        }
+
+        return ans;
+    }
+}
