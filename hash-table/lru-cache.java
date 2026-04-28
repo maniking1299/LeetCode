0f9b320a0bd5d64e103a6129570fd3d45c1f58ec
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-04-28 20:56:47 +0530

    LC-lru-cache [Runtime: 45 ms; Memory: 131.3 MB]

---
+++ hash-table/lru-cache.java
@@ -0,0 +1,77 @@
+class LRUCache {
+       class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int k, int v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    private int capacity;
+    private HashMap<Integer, Node> map;
+    private Node head, tail;
+
+
+    public LRUCache(int capacity) {
+         this.capacity = capacity;
+        map = new HashMap<>();
+
+        head = new Node(0, 0); 
+        tail = new Node(0, 0);
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insertToFront(Node node) {
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+    
+    public int get(int key) {
+           if (!map.containsKey(key)) return -1;
+
+        Node node = map.get(key);
+        remove(node);
+        insertToFront(node);
+
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
+         if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.value = value;
+
+            remove(node);
+            insertToFront(node);
+        } else {
+            if (map.size() == capacity) {
+                Node lru = tail.prev;
+                remove(lru);
+                map.remove(lru.key);
+            }
+
+            Node newNode = new Node(key, value);
+            insertToFront(newNode);
+            map.put(key, newNode);
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
