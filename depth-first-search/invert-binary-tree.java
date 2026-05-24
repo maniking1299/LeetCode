2d4ec6c8451aa06828a2b45a0cbf1e40c876a7c4
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-05-24 09:43:36 +0530

    LC-invert-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/invert-binary-tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        if(root == null) return root;
+
+        queue.offer(root);
+
+    while(!queue.isEmpty()){
+        TreeNode x= queue.poll();
+    
+            TreeNode temp = x.left;
+            x.left = x.right;
+            x.right = temp;
+            temp =null;
+
+        if(x.left!=null){
+                queue.offer(x.left);
+            }
+            if(x.right!=null){
+                queue.offer(x.right);
+            }
+    }    
+        return root;
+    }
+}
