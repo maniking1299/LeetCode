27b96124ec703673c8d742b0b62ea746ee21fa17
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-06-01 21:21:26 +0530

    LC-binary-tree-right-side-view [Runtime: N/A; Memory: N/A]

---
+++ tree/binary-tree-right-side-view.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    // TC -O(N)
+    // SC -O(N)
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if(root == null){
+            return ans; 
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+
+            int size = queue.size();
+
+            for(int i=0 ;i<size;i++){
+                TreeNode curr = queue.poll();
+                if(curr.left != null){
+                    queue.offer(curr.left);
+                }
+                if(curr.right != null){
+                    queue.offer(curr.right);
+                }
+
+                if(i == size-1){
+                    ans.add(curr.val);
+                }
+            }    
+
+        }
+
+        return ans;
+    }
+}
