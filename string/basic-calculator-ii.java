0bc7540a545dd1eb7fd1aa5cf1b1467e54b9d2a9
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-07-16 22:00:57 +0530

    LC-basic-calculator-ii [Runtime: N/A; Memory: N/A]

--- string/basic-calculator-ii.java
+++ string/basic-calculator-ii.java
@@ -1,6 +1,47 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> st = new Stack<>();
+        // TC -> O(N)
+        // SC -> O(1)
+
+        int currNo = 0;
+        char sign = '+';
+        int res = 0;
+        int lastNo = 0;
+
+        char ar[] = s.toCharArray();
+
+        for (int i = 0; i < ar.length; i++) {
+            char currChar = ar[i];
+
+            if (Character.isDigit(currChar)) {
+                currNo = currNo * 10 + currChar - '0';
+            }
+
+            if (!Character.isDigit(currChar) && currChar != ' ' || i == ar.length - 1) {
+                if (sign == '+' || sign == '-') {
+                    res += lastNo;
+                    lastNo = (sign == '+') ? currNo : -currNo;
+                } else if (sign == '*') {
+                    lastNo *= currNo;
+                } else if (sign == '/') {
+                    lastNo /= currNo;
+                }
+
+                sign = currChar;
+                currNo = 0;
+            }
+
+        }
+
+        return res+lastNo;
+
+    }
+}
+
+/*
+TC-> O(N)
+SC-> O(N)
+  Stack<Integer> st = new Stack<>();
 
         int currNo = 0;
         char op = '+';
@@ -39,5 +80,4 @@ class Solution {
 
         return res;
 
-    }
-}
+*/
