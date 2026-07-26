096c62946188d70b4c9558127c0073424b751dfd
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-07-26 22:37:57 +0530

    LC-maximum-product-of-three-numbers [Runtime: N/A; Memory: N/A]

--- sorting/maximum-product-of-three-numbers.java
+++ sorting/maximum-product-of-three-numbers.java
@@ -1,17 +1,33 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-      int i=0;
-      int j=2;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
 
-      int maxProduct = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
 
-      while(j<nums.length){
-        int currProduct = nums[i]*nums[i+1]*nums[j];
-        maxProduct = Math.max(currProduct,maxProduct);
-        i++;
-        j++;
-      }
+        for (int num : nums) {
 
-      return maxProduct;
+            if (num >= max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num >= max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num >= max3) {
+                max3 = num;
+            }
+
+            if (num <= min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num <= min2) {
+                min2 = num;
+            }
+        }
+
+        return Math.max(max1 * max2 * max3, min1 * min2 * max1);
     }
 }
