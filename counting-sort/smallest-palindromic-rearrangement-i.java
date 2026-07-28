108c11d5a3c302f297004fe25dc1afe58cf466f4
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-07-28 11:58:55 +0530

    LC-smallest-palindromic-rearrangement-i [Runtime: N/A; Memory: N/A]

---
+++ counting-sort/smallest-palindromic-rearrangement-i.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String smallestPalindrome(String s) {
+
+        if (s.length() == 1) {
+            return s;
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        int mid = s.length() / 2;
+
+        String half = s.substring(0, mid);
+
+        char[] arr = half.toCharArray();
+        Arrays.sort(arr);
+
+        half = new String(arr);
+
+        ans.append(half);
+
+        if ((s.length() % 2) == 1) {
+            ans.append(s.charAt(mid));
+        }
+
+        ans.append(new StringBuilder(half).reverse());
+
+        return ans.toString();
+    }
+
+}
