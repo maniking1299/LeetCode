342f0ecafc173f3a922dd03a15cd3193bdd6bc81
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-08-02 09:18:00 +0530

    LC-stone-game [Runtime: N/A; Memory: N/A]

--- array/stone-game.java
+++ array/stone-game.java
@@ -1,27 +1,40 @@
 class Solution {
+    int[][] t = new int[501][501];
+
     public boolean stoneGame(int[] piles) {
 
         int n = piles.length;
+
+        for (int[] row : t) {
+            Arrays.fill(row, -1);
+        }
+
         int total = 0;
-        for(int i:piles){
+        for (int i : piles) {
             total += i;
         }
 
-        int alice = solve(0,n-1,piles);
-        int bob = total-alice;
+        int alice = solve(0, n - 1, piles);
+        int bob = total - alice;
 
-        return alice>bob;
+        return alice > bob;
 
     }
 
-    public int solve(int i , int j ,int[] piles){
+    public int solve(int i, int j, int[] piles) {
 
-        if(i>j) return 0;
-        if(i == j) return piles[i];
+        if (i > j)
+            return 0;
+        if (i == j)
+            return piles[i];
 
-        int take_i = piles[i]+Math.min(solve(i+2,j,piles) ,solve(i+1,j-1,piles));
-        int take_j = piles[j]+Math.min(solve(i+1,j-1,piles) , solve(i,j-2,piles));
+        if (t[i][j] != -1) {
+            return t[i][j];
+        }
 
-        return Math.max(take_i,take_j);
+        int take_i = piles[i] + Math.min(solve(i + 2, j, piles), solve(i + 1, j - 1, piles));
+        int take_j = piles[j] + Math.min(solve(i + 1, j - 1, piles), solve(i, j - 2, piles));
+
+        return t[i][j] = Math.max(take_i, take_j);
     }
 }
