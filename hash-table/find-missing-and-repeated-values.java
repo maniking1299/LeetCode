77040a1b72bbf079c8e7559c8385896803930d1f
Manish Kumar <137608251+maniking1299@users.noreply.github.com>
2026-09-12 09:35:25 +0530

    LC-find-missing-and-repeated-values [Runtime: N/A; Memory: N/A]

---
+++ hash-table/find-missing-and-repeated-values.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+
+        int n = grid.length;
+
+        int freq[] = new int[n*n];
+
+   // Making a freq array
+
+        for(int i= 0 ; i<n ;i++){
+            for(int j=0 ; j<n ;j++){
+                int no = grid[i][j];
+                freq[no-1]++;
+            }
+        }
+
+        int a=0,b=0;
+
+        for(int i=0 ; i<freq.length ;i++){
+            if(freq[i] >1){
+                a = i+1;
+            }
+            if(freq[i] == 0){
+                b = i+1;
+            }
+        }
+
+        return new int[]{a,b};
+
+
+    }
+}
